13a9eee23afc9877483f2dca61bb5b08a57bdfc4
Jakub Ryška <ryskajakub@seznam.cz>
2012-12-04 20:32:49 +0100

    added right side list #13

--- wp-content/themes/dzogchen/articlelist.php
+++ wp-content/themes/dzogchen/articlelist.php
@@ -1,18 +1,17 @@
 <div id="right-articles">
-    <h2>Nejnovější články</h2>
-    <div class="right-box-inner scroll-pane">
+	<h2>Nejnovější články</h2>
 	<ul>
 		<?php
-		$recentPosts = new WP_Query();
-		$recentPosts->query('showposts=5');
-		while ($recentPosts->have_posts()): $recentPosts->the_post();
-		    ?>
-			<li>
-		    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-		    <span class="published"><?php echo get_the_date('j.n.Y'); ?></span>
-		    <?php the_excerpt(); ?> 
-			</li>
-		<?php endwhile; ?>
+			$recentPosts = new WP_Query();
+			$recentPosts->query('showposts=5');
+			while ($recentPosts->have_posts()): $recentPosts->the_post();
+		?>
+		<li>
+			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<span class="published"><?php echo get_the_date('j.n.Y'); ?></span>
+		</li>
+		<?php 
+			endwhile; 
+		?>
 	</ul>
-    </div>
 </div>
